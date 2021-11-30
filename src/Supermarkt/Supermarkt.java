c2c7901c6b56ba97cdc3f2a23977ee65da8a1efb
DaCas <DaCas@DESKTOP-CK9FBES>
2021-11-30 11:46:38 +0100

    test

--- src/Supermarkt/Supermarkt.java
+++ src/Supermarkt/Supermarkt.java
@@ -4,6 +4,10 @@ public class Supermarkt {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		for(int i=0; i<10; i++) {
+			System.out.println(i);
+		}
 			System.out.println("lololo");
+			
 	}
 }
