55d9c5743b707aec577296f2274a1b9837751534
Masteax <masteaxyt@gmail.com>
2021-12-01 15:51:35 +0100

    Objects created

---
+++ src/Objects/ShoppingCart/ShoppingCartItem.java
@@ -0,0 +1,8 @@
+package Objects.ShoppingCart;
+
+import java.util.List;
+
+public class ShoppingCartItem {
+    private List<ShoppingItem> PR_LSI_ShoppingCart;
+    private ShoppingCartEnumeration PR_SCE_ShoppingCartType;
+}
---
+++ src/Objects/ShoppingCart/ShoppingItem.java
@@ -0,0 +1,23 @@
+package Objects.ShoppingCart;
+
+import Objects.Goods.Good;
+
+public class ShoppingItem {
+    private final Good PR_G_Item;
+    private int PR_I_Amount;
+
+    public ShoppingItem(Good T_G_Item, int T_I_Amount) {
+       PR_G_Item = T_G_Item;
+       PR_I_Amount = T_I_Amount;
+    }
+
+    public int getPR_I_Amount() {
+        return PR_I_Amount;
+    }
+
+    public void setPR_I_Amount(int PR_I_Amount) {
+        this.PR_I_Amount = PR_I_Amount;
+    }
+
+
+}
